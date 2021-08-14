567ac79df55cfbc0e7d91a7b77fb09ee03d83703
root <1658783310@qq.com>
2021-08-14 20:03:20 +0800

    autowired和aware回调

---
+++ src/main/java/com/Context/BeanNameAware.java
@@ -0,0 +1,2 @@
+package com.Context;public @interface BeanNameAware {
+}
---
+++ src/main/java/com/wangyu/OrderService.java
@@ -0,0 +1,7 @@
+package com.wangyu;
+
+import com.Context.Component;
+
+@Component("orderService")
+public class OrderServive {
+}
